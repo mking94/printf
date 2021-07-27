5ac9417935ea10a7c40532e3c965df5fdc01ada7
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-27 18:33:19 +0200

    Header file

--- holberton.h
+++ holberton.h
@@ -1,19 +1,10 @@
-
 #ifndef PRINTF_H
 #define PRINTF_H
-#include <limits.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <unistd.h>
-
 /* functions prototype */
 int _printf(const char *format, ...);
 int _putchar(char c);
-
 /* functions prototypes conversion */
-int print_char(va_list arg);
-int print_string(va_list arg);
-int print_percent(va_list arg);
-
-
+int print_char(char arg);
+int print_string(char *arg);
+int print_int(int arg);
 #endif/* PRINTF_H */
