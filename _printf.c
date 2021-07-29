6c9d341ce74a85075b99ef7f51f234cbded7d846
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-29 11:05:10 +0100

    print all

--- _printf.c
+++ _printf.c
@@ -25,20 +25,20 @@ else if(format[i+1] == 's'){
 len = len + print_string(va_arg(arg,char *));
 i = i + 2;
 }
- else if(format[i+1] == 'b'){
-len=len+print_int(tobin(va_arg(arg,int));
+else if(format[i+1] == 'b'){
+len=len + print_int(tobin(va_arg(arg,int));
 i = i + 2;
 }
-  else if(format[i+1] == 'o'){
-len=len+print_int(tooct(va_arg(arg,int));
+else if(format[i+1] == 'o'){
+len=len + print_int(tooct(va_arg(arg,int));
 i = i + 2;
 }
 else if(format[i+1] == 'x'){
-len=len+print_int(tohex(va_arg(arg,int));
+len=len + print_string(tohex(va_arg(arg,int));
 i = i + 2;
 }
 else if(format[i+1] == 'X'){
-len=len+print_int(toHEX(va_arg(arg,int));
+len=len + print_string(toHEX(va_arg(arg,int));
 i = i + 2;
 }
 else if(format[i+1] == '%'){
