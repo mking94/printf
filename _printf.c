fe53b8154b44f3fbed9e9fa004f5f43aaf4b976d
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2021-07-30 11:53:19 +0100

    pritnall

--- _printf.c
+++ _printf.c
@@ -4,53 +4,63 @@
  * @format: format specifier
  * Return: count
  */
-int _printf(const char *format, ...) {
-int i = 0 ;
+int _printf(const char *format, ...)
+{
+int i = 0;
 int len = 0;
 va_list arg;
-va_start(arg,format);
-while(format[i]!='\0')
+va_start(arg, format);
+while (format[i] != '\0')
 {
-if(format[i] == '%')
+if (format[i] == '%')
 {
-if(format[i+1] == 'd' || format[i+1] == 'i'){
-len = len + print_int((int)va_arg(arg,int));
+if (format[i + 1] == 'd' || format[i + 1] == 'i')
+{
+len = len + print_int((int)va_arg(arg, int));
 i = i + 2;
 }
-else if(format[i+1] == 'u'){
-len = len + print_unsigned_int(va_arg(arg,unsigned int));
+else if (format[i + 1] == 'u')
+{
+len = len + print_unsigned_int(va_arg(arg, unsigned int));
 i = i + 2;
 }
-else if(format[i+1] == 'c'){
-len = len + print_char((char)va_arg(arg,int));
+else if (format[i + 1] == 'c')
+{
+len = len + print_char((char)va_arg(arg, int));
 i = i + 2;
 }
-else if(format[i+1] == 's'){
-len = len + print_string(va_arg(arg,char *));
+else if (format[i + 1] == 's')
+{
+len = len + print_string(va_arg(arg, char *));
 i = i + 2;
 }
-else if(format[i+1] == 'b'){
-len=len + print_int(tobin(va_arg(arg,unsigned int)));
+else if (format[i + 1] == 'b')
+{
+len = len + print_int(tobin(va_arg(arg, unsigned int)));
 i = i + 2;
 }
-else if(format[i+1] == 'o'){
-len=len + print_int(tooct(va_arg(arg,int)));
+else if (format[i + 1] == 'o')
+{
+len = len + print_int(tooct(va_arg(arg, int)));
 i = i + 2;
 }
-else if(format[i+1] == 'x'){
-len=len + print_string(tohex(va_arg(arg,int)));
+else if (format[i + 1] == 'x')
+{
+len = len + print_string(tohex(va_arg(arg, int)));
 i = i + 2;
 }
-else if(format[i+1] == 'X'){
-len=len + print_string(toHEX(va_arg(arg,int)));
+else if (format[i + 1] == 'X')
+{
+len = len + print_string(toHEX(va_arg(arg, int)));
 i = i + 2;
 }
-else if(format[i+1] == '%'){
-len=len+_putchar('%');
+else if (format[i + 1] == '%')
+{
+len = len + _putchar('%');
 i = i + 2;
 }
-} 
-else 
+}
+else
 {
 len = len + print_char(format[i]);
 i++;
